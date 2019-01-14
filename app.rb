08cfa1924ed43a9b4e09b4d368fa3f7e96518ce4
Anuj Panchal <anujpan1@gmail.com>
2019-01-14 18:43:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,13 +12,9 @@ module FormsLab
     end
 
     post '/pirates' do
-<<<<<<< HEAD
       @pirate = Pirate.new(params[:pirate])
       params[:pirate][:ships].each { |ship| Ship.new(ship)}
       erb :'/pirates/show'
-=======
-      erb :'pirates/show'
->>>>>>> 00ae6f905538be7ba7020b470e98296a1f3cdc65
     end
   end
 end
